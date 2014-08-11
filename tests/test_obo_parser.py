f5f8a53d6de58a384984bfa17e2d016fb4b74487
Kenta Sato <bicycle1885@gmail.com>
2014-08-12 00:57:11 +0900

    add OBOReader iteration test

--- tests/test_obo_parser.py
+++ tests/test_obo_parser.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from goatools.obo_parser import (
     OBOReader,
@@ -8,7 +9,17 @@ from goatools.obo_parser import (
 
 class TestOBOReader(unittest.TestCase):
     def test_iteration(self):
-        pass
+        reader = OBOReader(os.path.join(os.path.dirname(__file__), "data/mini_obo.obo"))
+        n = 0
+
+        for i, term in enumerate(reader, start=1):
+            n += 1
+            self.assertIsInstance(term, GOTerm)
+            # the GO IDs are sequential in this case
+            self.assertEqual(term.id, "GO:{:07d}".format(i))
+
+        # contains 10 records
+        self.assertEqual(n, 10)
 
 
 class TestGOTerm(unittest.TestCase):
